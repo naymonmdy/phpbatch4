e22d57df150f5b4ab6dade116b4f8fb4a489beae
naymonmdy <naymon2021@gmail.com>
2024-05-07 23:13:13 +0630

    regular expression day 2

--- part1/l23regularexpression.php
+++ part1/l23regularexpression.php
@@ -25,8 +25,8 @@ $result =preg_match("/we$/",$string);//false
 
 // ^ must be start case sensitvive ,caret or circumfrence
 $result =preg_match("/^mily/",$string);//false
-$result =preg_match("/^We/",$string);//false
-$result =preg_match("/^we/",$string);//true
+$result =preg_match("/^we/",$string);//false
+$result =preg_match("/^We/",$string);//true
 $result =preg_match("%^are%",$string);//false
 $result =preg_match("%^are#",$string);//false
 $result =preg_match("!^We!",$string);//true
@@ -47,6 +47,7 @@ $string ="my lucky number is 567";
 $result =preg_match("/[0-4]/",$string);//false
 $result =preg_match("/[5-9]/",$string);//true
 $result =preg_match("/[A-Z]|[a-z]/",$string);//true
+$result =preg_match("/[A-Z,a-z]/",$string);//true
 $result =preg_match("/[A-Z]/",$string);//false
 $result =preg_match("/[a-z]$/",$string);//false
 $result =preg_match("/^[a-z]/",$string);//false
@@ -57,6 +58,79 @@ $result =preg_match("/[5-9]$/",$string);//true
 $result =preg_match("/[^5-9]/",$string);//true it's mean that not include 5 to 9 of other character space , or number
 $result =preg_match("/[^a-z]/",$string);//true it's mean that not include a to Z of other character space , or number
 
+
+
+$string ="admin@gmail.com";
+
+$result=preg_match("/@/",$string);//true
+$result=preg_match("/m/",$string);//true
+$result=preg_match("/m+/",$string);//true
+$result=preg_match("/b+/",$string);//false
+$result=preg_match("/b*/",$string);//true
+$result=preg_match("/b?/",$string);//true
+
+
+$result=preg_match("/m{1}/",$string);//true
+$result=preg_match("/m{2}/",$string);//true
+
+
+$string ="adminn@gmail.com";
+$result=preg_match("/n{1}/",$string);//true
+$result=preg_match("/n{2}/",$string);//true
+$result=preg_match("/n{3}/",$string);//false
+
+$result=preg_match("/n{2,3}/",$string);//true
+$result=preg_match("/n{2,}/",$string);//true
+
+
+$string ="V8 Engine";
+$result=preg_match("/\s/",$string);//true space
+$result=preg_match("/\d/",$string);//true digit
+$result=preg_match("/\D/",$string);//true no digit
+$result=preg_match("/\w/",$string);//true word except special character
+$result=preg_match("/\W/",$string);//false special character $#@
+
+$string ="admin@gmail.com";
+$result=preg_match("/a\wm/",$string);//true 
+$result=preg_match("/a\w{1}m/",$string);//true 
+$result=preg_match("/a\w{2}m/",$string);// false any 2 exact word   =>adbmin@gmail.com
+$result=preg_match("/a\w{2,4}m/",$string);// false any 2  to 4 exact word =>adbcdmin@gmail.com
+$result=preg_match("/a\w{2,}m/",$string);// false any 2 to more exact word  =>a........min@gmail.com
+
+
+$result=preg_match("/a.m/",$string);// true c
+$result=preg_match("/a..m/",$string);// false 
+$result=preg_match("/a.{1}m/",$string);// true
+$result=preg_match("/a.{2}m/",$string);// false    =>adbmin@gmail.com
+$result=preg_match("/a.{2,4}m/",$string);// false  =>adbcdmin@gmail.com
+$result=preg_match("/a.{2,}m/",$string);// false   =>a........min@gmail.com c
+
+$string="PHP";
+$result=preg_match("/.{2}/",$string);//true
+
+$result=preg_match("/hp/",$string);//true
+$result=preg_match("/p(hp)*/",$string);//true
+$result=preg_match("/p(hp)+/",$string);//true
+
+
+$string ="Vv";
+$result=preg_match("/.{2}/",$string);//true
+$result=preg_match("/^.{2}$/",$string);//exact 2 character
+
+
+$string="Welcome to our <i>programming class</i>";
+
+$result=preg_match("/<i><\/i>/",$string);//false
+$result=preg_match("/<i>\w<\/i>/",$string);//false one char
+$result=preg_match("/<i>\w*<\/i>/",$string);//false contained space
+$result=preg_match("/<i>.<\/i>/",$string);//false one char
+$result=preg_match("/<i>.*<\/i>/",$string);//true
+$result=preg_match("/<i>(.*)<\/i>/",$string);//true
+
+
+$string ="admin@gmail.com";
+$result=preg_match("/^[A-z,a-z]+@[a-z]+\.\w{3}/",$string);//true this code to be email address or nor
+
 //  $ must be in end (case sensitive) -->
 // [] range a-z  A-Z 0-9
 // m+ must contain al least one m and more
@@ -64,18 +138,18 @@ $result =preg_match("/[^a-z]/",$string);//true it's mean that not include a to Z
 // m? can contain b or not and more
 // i use for not case sensitive
 // [a-f] use contain from a to f contain
-// m{nth} contain place m as per nth and more
-// m{nth,nth} contain place m as per nth and more
-// m{nth,} contain place m as per nth and more
+// m{nth} contain place m as per nth and exact time
+// m{nth,nth} contain place m as per nth from max to
+// m{nth,} contain place m as per nth and from to max to
 // \s space
 // \d digit
 // \D no digit
 // \w word [a-z][A-Z][0-9]
-// \W any word #@!
+// \W any #@!*
 // . any character
 // () this
 // + must
-// * can
+// * any can be contain or not
 // p(hp)*   can be contain hp
 // p(hp)+   must be hp
 // [[:space:]] space
